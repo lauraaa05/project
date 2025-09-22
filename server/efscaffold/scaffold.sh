8cbc0463311527f78bebc62580700b74156dc034
lauraaa05 <laushp01@easv365.dk>
2025-09-22 18:45:08 +0200

    scaffolding

---
+++ server/efscaffold/scaffold.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+set -a
+source .env
+set +a
+
+dotnet tool install -g dotnet-ef
+dotnet ef dbcontext scaffold "$CONN_STR" Npgsql.EntityFrameworkCore.PostgreSQL \
+    --output-dir ./Entities \
+    --context-dir . \
+    --context MyDbContext \
+    --no-onconfiguring \
+    --namespace efscaffold.Entities \
+    --context-namespace Infrastructure.Postgres.Scaffolding \
+    --schema project \
+    --force
---
+++ server/efscaffold/schema.sql
@@ -0,0 +1,11 @@
+﻿drop schema if exists project cascade;
+create schema if not exists project;
+
+create table project.todo(
+                             id text not null primary key,
+                             title text not null,
+                             description text not null,
+                             priority numeric not null,
+                             isdone boolean not null);
+
+
