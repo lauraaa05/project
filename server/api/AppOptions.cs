55c44a71db5b628d04a05ffe7906504500364f0e
lauraaa05 <laushp01@easv365.dk>
2025-09-25 15:52:53 +0200

    deployment

---
+++ server/api/AppOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public class AppOptions
+{
+    [MinLength(1)]
+    public string DbConnectionString { get; set; }
+    [MinLength(1)]
+    public string JwtSecret { get; set; }
+}
--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,19 +1,27 @@
+using api;
 using efscaffold.Entities;
 using Infrastructure.Postgres.Scaffolding;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
-    conf.UseNpgsql();
+    conf.UseNpgsql(appOptions.DbConnectionString);
 });
 
 var app = builder.Build();
 
-app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
+app.MapGet("/", (
+    
+    [FromServices]IOptionsMonitor<AppOptions> optionsMonitor,
+    [FromServices]MyDbContext dbContext) =>
 {
+    
     var myTodo = new Todo()
     {
         Description = "test",
